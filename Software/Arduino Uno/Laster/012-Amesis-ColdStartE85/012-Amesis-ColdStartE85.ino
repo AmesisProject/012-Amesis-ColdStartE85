a634ee7dea5a96b67cd8399f34c3fe0635585a1e
AmesisProject <57772906+AmesisProject@users.noreply.github.com>
2022-08-25 22:31:20 +0200

    Update 012-Amesis-ColdStartE85.ino
    Debut de creation du fichier

--- Software/Arduino Uno/Laster/012-Amesis-ColdStartE85/012-Amesis-ColdStartE85.ino
+++ Software/Arduino Uno/Laster/012-Amesis-ColdStartE85/012-Amesis-ColdStartE85.ino
@@ -1,9 +1,13 @@
+//012-Amesis-ColdStartE85 
+//v0.01 
+//25/08/2022
+
+
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
 }
