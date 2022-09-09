4d89d631d4092f9bc93be137c259b2bfde203fb8
AmesisProject <57772906+AmesisProject@users.noreply.github.com>
2022-09-09 09:51:43 +0200

    impor de la savegarde

---
+++ Software/Arduino Uno/Laster/012-Amesis-ColdStartE85/012-Amesis-ColdStartE85.ino
@@ -0,0 +1,13 @@
+//012-Amesis-ColdStartE85 
+//v0.02 
+//25/08/2022
+
+
+
+void setup() {
+
+}
+
+void loop() {
+ 
+}
---
+++ Software/Arduino Uno/Old/v0.01/012-Amesis-ColdStartE85/012-Amesis-ColdStartE85.ino
@@ -0,0 +1,13 @@
+//012-Amesis-ColdStartE85 
+//v0.01 
+//25/08/2022
+
+
+
+void setup() {
+
+}
+
+void loop() {
+ 
+}
